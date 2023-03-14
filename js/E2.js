b742fd083ce81849b00dcf52152783a360158217
leandro <leandro_navarro@outlook.es>
2023-03-14 12:49:53 -0300

    Agregue el segundo ejercicio resuelto

--- js/E2.js
+++ js/E2.js
@@ -9,3 +9,38 @@
 // ● Sustituye el elemento que ocupa la segunda posición por la ciudad de
 // 'Barcelona'.
 
+
+var ciudades = [];
+
+
+while (true) {
+  var ciudad = prompt("Ingresa el nombre de una ciudad:");
+  if (ciudad == null) {
+    break;
+  }
+  ciudades.push(ciudad);
+}
+
+document.write("el arreglo al comiendo es: ", ciudades);
+document.write("<br>")
+
+document.write("La longitud del arreglo es: " + ciudades.length);
+document.write("<br>")
+
+
+document.write("Los ítems en las posiciones primera, tercera y última son: " + ciudades[0] + ", " + ciudades[2] + ", " + ciudades[ciudades.length-1]);
+document.write("<br>")
+
+
+ciudades.push("París");
+
+
+document.write("El elemento en la segunda posición es: " + ciudades[1]);
+document.write("<br>")
+
+
+ciudades[1] = "Barcelona";
+
+
+document.write("El arreglo final es: " + ciudades);
+document.write("<br>")
