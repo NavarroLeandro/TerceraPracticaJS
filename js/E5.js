2d2b7432e12a1467c5e829ba764114f9863afa58
leandro <leandro_navarro@outlook.es>
2023-03-14 15:28:03 -0300

    Hechos todos los ejercicios y testeados

--- js/E5.js
+++ js/E5.js
@@ -2,3 +2,21 @@
 // le pasa como argumento. A partir de la cadena que se le pasa, la función
 // determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o
 // por una mezcla de ambas.
+
+var cadenaDeCaracteres;
+
+function analizarCadena(cadena) {
+  if (cadena === cadena.toUpperCase()) {
+    return document.write("La cadena está formada solo por mayúsculas.");
+  } else if (cadena === cadena.toLowerCase()) {
+    return document.write("La cadena está formada solo por minúsculas.");
+  } else {
+    return document.write("La cadena está formada por una mezcla de mayúsculas y minúsculas.");
+  }
+}
+
+cadenaDeCaracteres = prompt(
+  "Ingrese una frase u oracion en mayuscula o minuscula o como usted quiera: "
+);
+
+analizarCadena(cadenaDeCaracteres);
